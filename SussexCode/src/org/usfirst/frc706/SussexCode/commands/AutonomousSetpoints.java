da5c6de1c915d6cea7f0cf1c5ed5f2aad1103a74
juliavanhorn <juliajvanhorn@gmail.com>
2016-03-11 17:39:17 -0600

    Removed Extra Stuff from SmartDashboard

--- SussexCode/src/org/usfirst/frc706/SussexCode/commands/AutonomousSetpoints.java
+++ SussexCode/src/org/usfirst/frc706/SussexCode/commands/AutonomousSetpoints.java
@@ -33,13 +33,11 @@ public class AutonomousSetpoints extends Command {
     	shooterPos = shooter;
     }
 
-    // Called just before this Command runs the first time
     protected void initialize() {
     	RobotMap.shootershooterAngleDrive.changeControlMode(TalonControlMode.Position);
     	RobotMap.intakeintakeAngleDrive.changeControlMode(TalonControlMode.Position);
     }
 
-    // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	switch(shooterPos){
     	case "top":
@@ -70,17 +68,13 @@ public class AutonomousSetpoints extends Command {
     	}
     }
 
-    // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
         return done;
     }
 
-    // Called once after isFinished returns true
     protected void end() {
     }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
     protected void interrupted() {
     }
 }
